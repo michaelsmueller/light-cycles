e2bf12cb0ac105b79cb4be848154d0555dbcbf34
Michael Mueller <michael@miniguide.co>
2020-01-28 22:50:36 +0100

    paint lightcycle jetwall

--- js/game.js
+++ js/game.js
@@ -2,12 +2,28 @@
 
 class Game {
     constructor(ctx, canvas) {
+        console.log(`Canvas width ${canvas.width} height ${canvas.height}`);
         this.ctx = ctx;
         this.cellWidth = 10;
         this.rows = canvas.width / this.cellWidth;
         this.columns = canvas.height / this.cellWidth;
+        this.player1 = new LightCycle(canvas.width / this.cellWidth, canvas.height / this.cellWidth);
     }
 
+    _drawJetwall() {
+        this.ctx.fillStyle = "#75e7f1";
+        game.player1.jetwall.forEach(position => {
+          ctx.fillRect(position.column * this.cellWidth, position.row * this.cellWidth, 9, 9);
+        });
+    }
+    
+    _update() {
+        this._drawJetwall();
+        if (!!game.interval) {
+            this.interval = window.requestAnimationFrame(this._update.bind(this));
+        }
+    }
+    
     _assignControlsToKeys() {
         document.addEventListener('keydown', e => {
             console.log(`Keydown ${e.keyCode} ${e.key}`);
@@ -29,8 +45,8 @@ class Game {
     }
 
     start() {
-        this.player1 = new LightCycle(canvas.width / this.cellWidth, canvas.height / this.cellWidth);
-        this.player1.move();
+        game.player1.move();
+        this.interval = window.requestAnimationFrame(this._update.bind(this));
         this._assignControlsToKeys();
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 // jshint esversion: 6
 let ctx, game;
 let canvas = document.getElementById('game-grid');
+canvas.width = 800;
+canvas.height = 1000;
 ctx = canvas.getContext('2d');
 
 function start() {
-    console.log('Function start');
+    console.log('Instantiating game');
     game = new Game(ctx, canvas);
     game.start();
 }
@@ -12,7 +14,7 @@ function start() {
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM content loaded');
     document.getElementById('start').onclick = () => {
-        console.log('Start onClick');
+        console.log('onClick event fired');
         start();
     };
 });
