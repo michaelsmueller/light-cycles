5a0250aea05f4ef777fa6dd5746d3bac108bbdab
Michael Mueller <michael@miniguide.co>
2020-01-28 22:57:23 +0100

    LigthCycle turns

--- js/lightcycle.js
+++ js/lightcycle.js
@@ -12,17 +12,62 @@ class LightCycle {
       this.jetwall = [...this.jetwall];
       this.maxRows = maxRows;
       this.maxColumns = maxColumns;
-      this.direction = 'right';
+      this.direction = "right";
       this.intervalId = undefined;
     }
 
     _moveForward() {
         let cycle = this.jetwall[0];
-        // going right;
-        this.jetwall.unshift({
-            row: cycle.row,
-            column: (cycle.column + 1) % this.maxColumns
-        });
+        switch (this.direction) {
+          case "up":
+            this.jetwall.unshift({
+              row: (cycle.row - 1 + this.maxRows) % this.maxRows,
+              column: cycle.column
+            });
+            break;
+          case "down":
+            this.jetwall.unshift({
+              row: (cycle.row + 1) % this.maxRows,
+              column: cycle.column
+            });
+            break;
+          case "left":
+            this.jetwall.unshift({
+              row: cycle.row,
+              column: (cycle.column - 1 + this.maxColumns) % this.maxColumns
+            });
+            break;
+          case "right":
+            this.jetwall.unshift({
+              row: cycle.row,
+              column: (cycle.column + 1) % this.maxColumns
+            });
+            break;
+        }
+    }
+
+    goUp() {
+      if (this.direction === "left" || this.direction === "right") {
+        this.direction = "up";
+      }
+    }
+  
+    goDown() {
+      if (this.direction === "left" || this.direction === "right") {
+        this.direction = "down";
+      }
+    }
+  
+    goLeft() {
+      if (this.direction === "up" || this.direction === "down") {
+        this.direction = "left";
+      }
+    }
+  
+    goRight() {
+      if (this.direction === "up" || this.direction === "down") {
+        this.direction = "right";
+      }
     }
 
     move() {
