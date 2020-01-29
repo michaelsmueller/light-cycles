c6378f887a929638658b2854876be8ce594f5536
Michael Mueller <michael@miniguide.co>
2020-01-29 23:32:47 +0100

    attempt to add audio

--- js/main.js
+++ js/main.js
@@ -1,14 +1,40 @@
 // jshint esversion: 6
 
-let ctx, game;
+let ctx, game, audioContext, bufferLoader;
 let canvas = document.getElementById('game-grid');
 canvas.width = 900;
 canvas.height = 700;
 ctx = canvas.getContext('2d');
 
+
 function gameOver() {
     console.log("Game over");
     // add something cool here
+}
+
+function startAudio() {
+    document.AudioContext = document.AudioContext || document.webkitAudioContext;
+    audioContext = new AudioContext();
+    console.log('Audio context started');
+}
+
+function bufferAudio() {
+    bufferLoader = new BufferLoader(
+        audioContext,
+        [
+            "audio/the-grid.mp3"
+            // Daft Punk - The Grid (Joseph Darwed Orchestral Rework) [Tron Soundtrack] [HD 1080p]
+        ],
+        playAudio
+        );
+    bufferLoader.load();
+}
+
+function playAudio(bufferList) {
+    let music = audioContext.createBufferSource();
+    music.buffer = bufferList[0];
+    music.connect(audioContext.destination);
+    music.start(0);
 }
 
 function start() {
@@ -20,7 +46,9 @@ function start() {
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM content loaded');
     document.getElementById('start').onclick = () => {
-        console.log('onClick event fired');
+        console.log('Start game button clicked');
+        startAudio();
+        bufferAudio();
         start();
     };
 });
